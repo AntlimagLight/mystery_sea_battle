b0e72482648402c7326cdd5a5c6ab5a9ad1a8cb0
Ант <antlighter@gmail.com>
2025-01-07 04:41:34 +0300

    Добавлено логирование старта приложения

--- src/main/java/ru/misteriumfrpg/mystery_sea_battle/MysterySeaBattleApplication.java
+++ src/main/java/ru/misteriumfrpg/mystery_sea_battle/MysterySeaBattleApplication.java
@@ -1,12 +1,15 @@
 package ru.misteriumfrpg.mystery_sea_battle;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MysterySeaBattleApplication {
 
     public static void main(String[] args) {
+        log.debug("Начинается старт приложения");
         SpringApplication.run(MysterySeaBattleApplication.class, args);
     }
 
